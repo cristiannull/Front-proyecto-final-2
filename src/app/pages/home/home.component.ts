58fb5dcdc4d21913c7f94428f027b57b1c23a139
Ronald Stiven Beltran Rojas <ronaldbeltranrojas@gmail.com>
2024-05-24 15:11:21 -0500

    Small changes in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,31 +12,31 @@ import { NavComponent } from '../../components/nav/nav.component';
 export class HomeComponent {
   videogames = [
     {
-      title: 'Gears of war',
+      title: 'Gears of war 4',
       price: '20',
       image:
-        'https://th.bing.com/th/id/OIP.hhE4ub1fRWYfhZt2s6w80gHaEK?w=310&h=180&c=7&r=0&o=5&pid=1.7',
+        'https://www.neoteo.com/wp-content/uploads/2016/09/Gears-of-War-4-1.jpg',
     },
 
     {
-      title: 'Call of Duty II',
+      title: 'Call of Duty MW III',
       price: '35',
       image:
-        'https://th.bing.com/th/id/OIP.ryX85S4iHT64R2eRTPH4iwAAAA?w=172&h=180&c=7&r=0&o=5&pid=1.7',
+        'https://assets.xboxservices.com/assets/ff/26/ff265ed9-6adf-4618-997d-38b5beab7557.jpg?n=CoD-Modern-Warfare-III-PC-Battle.net-Standard_1440x2160.jpg',
     },
 
     {
       title: 'God of war',
       price: '100',
       image:
-        'https://th.bing.com/th/id/OIP.CWK-TTjEfrtFtYmul_H8jgHaEo?w=248&h=180&c=7&r=0&o=5&pid=1.7',
+      'https://cdn1.epicgames.com/offer/3ddd6a590da64e3686042d108968a6b2/EGS_GodofWar_SantaMonicaStudio_S2_1200x1600-fbdf3cbc2980749091d52751ffabb7b7_1200x1600-fbdf3cbc2980749091d52751ffabb7b7',
     },
 
     {
       title: 'Doom',
       price: '45',
       image:
-        'https://th.bing.com/th/id/OIP.Z2puH4WvbBz_i_w05Mj4UAHaEK?w=292&h=180&c=7&r=0&o=5&pid=1.7',
+        'https://image.api.playstation.com/vulcan/ap/rnd/202010/0114/b4Q1XWYaTdJLUvRuALuqr0wP.png',
     },
   ];
 }
