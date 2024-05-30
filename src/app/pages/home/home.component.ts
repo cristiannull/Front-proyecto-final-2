310d5e6168702d291afe83b118c5bc990ec7613c
Ronald Stiven Beltran Rojas <ronaldbeltranrojas@gmail.com>
2024-05-30 16:49:50 -0500

    create component footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { CardsComponent } from '../../components/cards/cards.component';
 import { NavComponent } from '../../components/nav/nav.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { VideogamesService } from '../../service/videogames.service';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardsComponent, NavComponent, RouterLinkActive, RouterLink],
+  imports: [CardsComponent, NavComponent, RouterLinkActive, RouterLink, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- src/app/pages/videogamelist/videogamelist.component.ts
+++ src/app/pages/videogamelist/videogamelist.component.ts
@@ -3,11 +3,12 @@ import { CardsComponent } from '../../components/cards/cards.component';
 import { NavComponent } from '../../components/nav/nav.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { VideogamesService } from '../../service/videogames.service';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-videogamelist',
   standalone: true,
-  imports: [CardsComponent, NavComponent, RouterLinkActive, RouterLink],
+  imports: [CardsComponent, NavComponent, RouterLinkActive, RouterLink, FooterComponent],
   templateUrl: './videogamelist.component.html',
   styleUrl: './videogamelist.component.css',
 })
