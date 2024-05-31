7fce84a7720af87b97edcbf20c6a07206a2bd6db
Cristian Gordon <coralcristian5@gmail.com>
2024-05-31 15:17:56 -0500

    fix style and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { VideogamelistComponent } from './pages/videogamelist/videogamelist.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'videogamelist', component: VideogamelistComponent },
 ];
