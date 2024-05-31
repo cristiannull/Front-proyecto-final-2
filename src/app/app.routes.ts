e075efb57186a74f3b4ad19866c3e197cd0da1f4
Dianadiaz <dianacarolinaa2024@gmail.com>
2024-05-31 15:10:57 -0500

    create page detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { VideogamelistComponent } from './pages/videogamelist/videogamelist.component';
-
+import { DetailComponent } from './pages/detail/detail.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'videogamelist', component: VideogamelistComponent },
+  { path: 'detail/:id', component: DetailComponent },
 ];
---
+++ src/app/models/videogame.models.ts
@@ -0,0 +1,13 @@
+export interface Videogame {
+    id: string; 
+    title: string; 
+    price: string;
+    image: string;
+    category: string;
+    developer: string;
+    gender: string;
+    pegi: string; 
+    theme: string;
+    paymentMethod: string;
+   
+  }
