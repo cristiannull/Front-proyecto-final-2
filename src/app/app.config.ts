4d38ee2b934414ebbecfb9cea946e9acfe6031d7
Ronald Stiven Beltran Rojas <ronaldbeltranrojas@gmail.com>
2024-07-30 18:43:02 -0500

    add nrgx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withInMemoryScrolling} from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding(), withInMemoryScrolling({ 
-      scrollPositionRestoration: "top",})),
+    provideRouter(routes, withComponentInputBinding(), withInMemoryScrolling({
+        scrollPositionRestoration: "top",
+    })),
     provideHttpClient(),
-  ],
+    provideStore()
+],
 };
